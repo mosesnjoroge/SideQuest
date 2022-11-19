d0150d4a1bd3be164939a10c335609c1c919ad8b
Alex Micu <alex.micu2012@yahoo.com>
2022-11-19 16:11:20 -0500

    added geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_19_204046) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_19_210737) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -68,6 +68,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_19_204046) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["category_id"], name: "index_side_quests_on_category_id"
     t.index ["user_id"], name: "index_side_quests_on_user_id"
   end
