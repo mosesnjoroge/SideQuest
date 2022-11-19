2e02e6a454067db99769ac59ca982220e15836fb
Alex Micu <alex.micu2012@yahoo.com>
2022-11-19 13:24:45 -0500

    completed the validations for all the models and the associations

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,4 +1,5 @@
 class Stop < ApplicationRecord
   belongs_to :trip
   belongs_to :side_quest
+  validates :order, numericality: { only_integer: true }
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,3 +1,5 @@
 class Trip < ApplicationRecord
   belongs_to :user
+  has_many :side_quests, through: :stops
+  has_many :stops, dependent: :destroy
 end
