df961b440b93173584cb2433326d47765929b67b
Alex Micu <alex.micu2012@yahoo.com>
2022-11-22 20:09:11 -0500

    fixed a bug for user image:"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,2 @@
 class UsersController < ApplicationController
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :encrypted_password, :reset_password_token, :avatar)
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :trips, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :side_quests, dependent: :destroy
-  has_one_attached :avatar
+  has_one_attached :photo
 
   # geocoded_by :address
   # after_validation :geocode, if:
