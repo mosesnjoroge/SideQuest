86b38b0276d6da61018dc77c4a5ad8dddae57f06
Ricardo Gutierrez <ricardorgvillanueva@gmail.com>
2022-11-24 21:14:05 -0500

    database seeded. Images missing

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,24 +44,123 @@ category = Category.create({
 })
 
 sidequest = SideQuest.create!({
-  name: "Zoo de Granby",
-  address: "4578 rue verdun",
+  name: "Parc Omega",
+  address: "399 QC-323, Montebello, QC J0V 1L0",
   category: category,
-  description: "One of the major zoos located in Canada and North America",
+  description: "Nature park featuring wildlife, picnicking, First Nations totems & an eatery, plus cabins & tipis.",
+  price: 50,
+  user: ricardo
+})
+
+
+sidequest2 = SideQuest.create!({
+  name: "Jessup's Falls Conservation Area",
+  address: "6675 County Rd 17, Plantagenet, ON K0B 1L0",
+  category: category,
+  description: "A Little Corner of Paradase",
+  price: 25,
+  user: ricardo
+})
+
+sidequest2 = SideQuest.create!({
+  name: "Papanack Park Zoo",
+  address: "150 County Rd 19, Wendover, ON K0A 3K0",
+  category: category,
+  description: "A Small Zoo in Wendover",
+  price: 50,
+  user: alex
+})
+
+
+sidequest2 = SideQuest.create!({
+  name: "Little Ray's Nature Centre",
+  address: "6675 County Rd 17, Plantagenet, ON K0B 1L0",
+  category: category,
+  description: "One of the largest exotic animal rescues in North America",
+  price: 100,
+  user: alex
+})
+
+
+sidequest2 = SideQuest.create!({
+  name: "Humanics Sanctuary and Sculpture Park",
+  address: "3468 Ch. Old Montréal Rd, Ottawa, ON K4C 1C8",
+  category: category,
+  description: "Come and experience this hidden jewel just outside Cumberland Village in Ottawa. ",
+  price: 5,
+  user: ricardo
+})
+
+
+
+sidequest2 = SideQuest.create!({
+  name: "Proulx Sugar Bush & Berry Farm",
+  address: "1865 O'Toole Rd, Cumberland, ON K4C 1N2",
+  category: category,
+  description: "
+  Since 1920, this family-owned farm has offered maple products & activities such as berry picking",
   price: 100,
   user: ricardo
 })
 
 
 sidequest2 = SideQuest.create!({
-  name: "Fairmont Le Château Frontenac",
-  address: " 1 Rue des Carrières, Québec, QC G1R 4P5",
+  name: "Calypso Theme Waterpark",
+  address: "2015 Calypso St, Limoges, ON K0A 2M0",
   category: category,
-  description: "The Château Frontenac is an excellent example of the grand hotels developed by railway companies in Canada in the late 1800s. Considered the world's most photographed hotel, it was designated a National Historic Site in 1981",
+  description: "100 acres of activities for all ages, including a giant wave pool, toddler area & fast waterslides.
+
+  ",
   price: 100,
   user: ricardo
 })
 
+sidequest2 = SideQuest.create!({
+  name: "Mariposa Farm",
+  address: "6468 County Rd 17, Plantagenet, ON K0B 1L0",
+  category: category,
+  description: "SUSTAINABLE AGRICULTURE, LOCAL FOOD AND RURAL LIFE IS WHAT WE’RE ALL ABOUT!",
+  price: 100,
+  user: ricardo
+})
+
+sidequest2 = SideQuest.create!({
+  name: "Kirkview Farms",
+  address: "20921 Laggan Glenelg Rd, Dalkeith, ON K0B 1E0",
+  category: category,
+  description: "Your farmers. Naturally",
+  price: 100,
+  user: ricardo
+})
+
+
+sidequest2 = SideQuest.create!({
+  name: "Cumberland Heritage Village Museum",
+  address: "2940 Ch. Old Montréal Rd, Cumberland, ON K4C 1G3",
+  category: category,
+  description: "Collection of structures depicting early 20th-century life from a general store to a train station.",
+  price: 100,
+  user: ricardo
+})
+
+
+sidequest2 = SideQuest.create!({
+  name: "Good Food Garden",
+  address: "1793 Pleasant Corner Rd, Vankleek Hill, ON K0B 1R0",
+  category: category,
+  description: "Heathy and Natural Soil. Healthy Plants. Heathy Food.",
+  price: 100,
+  user: ricardo
+})
+
+sidequest2 = SideQuest.create!({
+  name: "L'Orignal Schoolhouse #4",
+  address: "6675 County Rd 17, Plantagenet, ON K0B 1L0",
+  category: category,
+  description: "This is School house #4, which was built in the early 1900's. It is located in L'orignal/Cassburn Ontario. It was built for Franco Ontariens.",
+  price: 100,
+  user: ricardo
+})
 
 
 review = Review.create({
