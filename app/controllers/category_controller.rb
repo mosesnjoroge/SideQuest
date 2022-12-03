c11f563c168972030dd6a0c22efdeaae11375590
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-12-03 17:21:43 -0500

    category controller action new&create made for the form

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,6 +1,48 @@
 class CategoryController < ApplicationController
+  before_action :set_trip
+
+  def index
+    @categorys = Category.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
 
   def new
-    raise
+    @category = Category.new
   end
+
+  def create
+    @category = Category.new
+    @category.user = current_user
+    if @category.save
+      redirect_to trip_path(@trip)
+    else
+      redirect_to new_trip_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def category_params
+
+  end
+
+  def set_trip
+    @trip = Trip.find(params[:id])
+  end
+
 end
