e381000305cf28203fe69621710a4ab283a92a6f
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-12-03 18:42:23 -0500

    trips subform

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :side_quests, dependent: :destroy
 
   validates :name, presence: true
+  accepts_nested_attributes_for :side_quests, allow_destroy: true, reject_if: :all_blank
 end
