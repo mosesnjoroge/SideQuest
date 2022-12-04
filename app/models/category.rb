5f752b9558d7da1be0e8a964fde21c3081dffba9
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-12-04 14:53:14 -0500

    trips/new form

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,4 @@ class Category < ApplicationRecord
   has_many :side_quests, dependent: :destroy
 
   validates :name, presence: true
-  accepts_nested_attributes_for :side_quests, allow_destroy: true, reject_if: :all_blank
 end
